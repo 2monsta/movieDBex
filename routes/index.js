b75d757ca71b6b18e914d4320fb8d79978f9c24e
Jason Li <Yunf.li37@gmail.com>
2017-11-02 13:38:42 -0400

    update database and route to pug

--- routes/index.js
+++ routes/index.js
@@ -11,41 +11,50 @@ const imageBaseUrl = 'http://image.tmdb.org/t/p/w300';
 
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-	request.get(nowPlayingUrl, (error, response, movieData)=>{
-		if(error){
-			console.log(error);
-		}else{
-			var parsedData = JSON.parse(movieData);
-			res.render("test", {
-				parsedData: parsedData.results,
-				imageBaseUrl: imageBaseUrl
-			});
-		}
-	});
-    // res.render('index', { title: 'Express' });
-});
-// if you have /: that part of the path is WILD
+// router.get('/', function(req, res, next) {
+// 	request.get(nowPlayingUrl, (error, response, movieData)=>{
+// 		if(error){
+// 			console.log(error);
+// 		}else{
+// 			var parsedData = JSON.parse(movieData);
+// 			res.render("test", {
+// 				parsedData: parsedData.results,
+// 				imageBaseUrl: imageBaseUrl
+// 			});
+// 		}
+// 	});
+//     // res.render('index', { title: 'Express' });
+// });
+// // if you have /: that part of the path is WILD
 // in this case /movie/:movieID will trigger on  /movie/ANYTHING
 // to access the anything, you go to req.params.ANYTHING
-router.get("/movie/:movieID", (req, res)=>{
-	// somewhere in the movieAPI backend, they made some JSON then JSON.stringify
-	var movieID = req.params.movieID;
-	var thisMovieURL = `${apiBasedUrl}/movie/${movieID}?api_key=${config.apiKey}`
-	request.get(thisMovieURL, (error, response, movieData)=>{
-		var parsedData = JSON.parse(movieData);
-		res.json(parsedData);
-		// res.render("single-movie", {movieData: parsedData, imageBaseUrl: imageBaseUrl})
-	})
-})
-router.post("/search", (req,res)=>{
-	res.send("search route here");
-	// anything in a form that has a name send through post is available inside the req.body object
-	res.json(req.body); //will return the data back in json format
+// router.get("/movie/:movieID", (req, res)=>{
+// 	// somewhere in the movieAPI backend, they made some JSON then JSON.stringify
+// 	var movieID = req.params.movieID;
+// 	var thisMovieURL = `${apiBasedUrl}/movie/${movieID}?api_key=${config.apiKey}`
+// 	request.get(thisMovieURL, (error, response, movieData)=>{
+// 		var parsedData = JSON.parse(movieData);
+// 		// res.json(parsedData);
+// 		res.render("single-movie", {movieData: parsedData, imageBaseUrl: imageBaseUrl})
+// 	})
+// })
+// router.post("/search", (req,res)=>{
+// 	// res.send("search route here");
+// 	// anything in a form that has a name send through post is available inside the req.body object
+// 	// res.json(req.body); //will return the data back in json format
+	
 
-})
+// })
 
+
+router.get("/", (req, res, next)=>{
+	res.render("index");
+})
 // anything in a form that has name send through a get request, is available inside the req.query
 
-
+router.post("/registerProcess", (req, res, next)=>{
+	var name = req.body.name;
+	var email = req.body.email;
+	var password = req.body.password;
+})
 module.exports = router;
