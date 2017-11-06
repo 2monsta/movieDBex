46ae7200b99ea10f6f87dc687dadd5b1c80e8ac9
Jason Li <Yunf.li37@gmail.com>
2017-11-05 21:08:52 -0500

    update database to have different users

--- routes/index.js
+++ routes/index.js
@@ -223,35 +223,39 @@ router.get("/fav", (req, res, next)=>{
 		var movieTitle = req.query.movieTitle;
 		// step ONE
 			// check to see if it's already in the database
-		var checkQuery = "select title from favorite where title = ?;"; //check email and favorite is in the database
-		connection.query(checkQuery, [movieTitle], (error, results, field)=>{
-			if(results.length ==0){ //nothing is found in the database
-				var getuserIdQuery = 'select id from users where email =?;';
-				connection.query(getuserIdQuery, [email], (error,results,field)=>{
-					console.log(results);
-					var id = results[0].id;
-					var insertQuery = "insert into favorite (userID, imageUrl, title) values (?, ?, ?);";
-					console.log(id);
-					connection.query(insertQuery, [id, postPath, movieTitle], (error, results, field)=>{
-						var selectQ = "select f.title, f.imageUrl, f.userID from favorite as f join users as u on f.userID = u.id where u.email=? ;";
-						connection.query(selectQ, [email], (error, results, field)=>{
-							res.render("fav", {
-								baseImageUrl:imageBaseUrl,
-								result: results
+		var getID = "select id from users where email = ?;";
+		connection.query(getID, [email], (error, results, field)=>{
+			var userI = results[0].id;
+			var checkQuery = "select title from favorite where title = ? and userID = ?;"; //check email and favorite is in the database
+			connection.query(checkQuery, [movieTitle, userI], (error, results, field)=>{
+				if(results.length ==0){ //nothing is found in the database
+					var getuserIdQuery = 'select id from users where email =?;';
+					connection.query(getuserIdQuery, [email], (error,results,field)=>{
+						console.log(results);
+						var id = results[0].id;
+						var insertQuery = "insert into favorite (userID, imageUrl, title) values (?, ?, ?);";
+						console.log(id);
+						connection.query(insertQuery, [id, postPath, movieTitle], (error, results, field)=>{
+							var selectQ = "select f.title, f.imageUrl, f.userID from favorite as f join users as u on f.userID = u.id where u.email=? ;";
+							connection.query(selectQ, [email], (error, results, field)=>{
+								res.render("fav", {
+									baseImageUrl:imageBaseUrl,
+									result: results
+								});
 							});
 						});
 					});
-				});
-			}else{
-				var selectQ = "select f.title, f.imageUrl, f.userID from favorite as f join users as u on f.userID = u.id where u.email=? ;";
-				connection.query(selectQ, [email], (error, results, field)=>{
-					res.render("fav", {
-						baseImageUrl:imageBaseUrl,
-						result: results
+				}else{
+					var selectQ = "select f.title, f.imageUrl, f.userID from favorite as f join users as u on f.userID = u.id where u.email=? ;";
+					connection.query(selectQ, [email], (error, results, field)=>{
+						res.render("fav", {
+							baseImageUrl:imageBaseUrl,
+							result: results
+						});
 					});
-				});
-			}
-		});
+				}
+			});
+		})
 		// step TWO
 			// if it is, jsut render the page with whats in the db
 			// if not, insert it in to the page and render the page again
